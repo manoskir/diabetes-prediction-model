94556bb63702919f9522c6e134ce4de8cf181385
manoskir <em.kiriakakis@gmail.com>
2023-08-10 14:03:18 +0300

    Adding init.py and model training file

---
+++ model_training.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from dataprep import load_data
