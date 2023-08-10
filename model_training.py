0c677870a42e0d17d7f50cd8710621e7ec712c0d
manoskir <em.kiriakakis@gmail.com>
2023-08-10 23:09:36 +0300

    Adds a basic neural network to see how it runs

--- model_training.py
+++ model_training.py
@@ -4,3 +4,23 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from data_prep import load_data
+
+X_train, y_train, X_test, y_test, X_val, y_val = load_data("diabetes_prediction_dataset.csv")
+
+model = Sequential(
+    [
+        Dense(8, activation = 'relu', name = "L1"),
+        Dense(4, activation = 'relu', name = "L2"),
+        Dense(2, activation = 'linear', name = "L3")
+    ]
+)
+
+model.compile(
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    optimizer=tf.keras.optimizers.Adam(0.01), metrics=['accuracy']
+)
+
+model.fit(
+    dd1_X_train,dd1_y_train,
+    epochs=10, validation_data=(dd1_X_test,dd1_y_test)
+)
