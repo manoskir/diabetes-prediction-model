809ae0cd2e45c9b5e02317310a2f79b5efbe21b7
manoskir <em.kiriakakis@gmail.com>
2023-08-11 13:57:08 +0300

    Adds initial code for model architecture creation
    This is the part that will help construct model architectures so that they will be fed later on to the model creation and optimization algo

--- model_training.py
+++ model_training.py
@@ -7,6 +7,20 @@ from data_prep import load_data
 
 X_train, y_train, X_test, y_test, X_val, y_val = load_data("diabetes_prediction_dataset.csv")
 
+#here's the helper code that will help me construct model architectures for model training optimization
+num_layers = 2
+min_num_nodes = 3
+max_num_nodes = 7
+
+node_options = list(range(min_num_nodes, max_num_nodes+1))
+
+layer_possibilities = num_layers*[node_options]
+
+layer_node_permutations = list(itertools.product(*layer_possibilities))
+print(layer_node_permutations)
+
+
+'''
 model = Sequential(
     [
         Dense(8, activation = 'relu', name = "L1"),
@@ -24,3 +38,4 @@ model.fit(
     X_train,y_train,
     epochs=10, validation_data=(X_test,y_test)
 )
+'''
