2a3f56d7a764f654f82e996837ab8f70a5aea28d
manoskir <em.kiriakakis@gmail.com>
2023-08-13 22:58:52 +0300

    Adds get_models function
    The function that produces the models that will be fed to the optimization function based on the params specified by the user

--- model_training.py
+++ model_training.py
@@ -9,34 +9,49 @@ from data_prep import load_data
 X_train, y_train, X_test, y_test, X_val, y_val = load_data("diabetes_prediction_dataset.csv")
 
 #here's the helper code that will help me construct model architectures for model training optimization
-num_layers = 2
-min_num_nodes = 3
-max_num_nodes = 7
+def get_models(num_layers: int,
+              min_num_nodes: int,
+              max_num_nodes: int,
+              node_step_size: int,
+              hidden_layer_activation: str = 'relu',
+              num_nodes_at_output: int = 2,
+              output_layer_activation: str = 'linear') -> list:
+    
+    node_options = list(range(min_num_nodes, max_num_nodes+1))
+    layer_possibilities = num_layers*[node_options]
+    layer_node_permutations = list(itertools.product(*layer_possibilities))
+    
+    #list that holds all the models that will be created based on the nodes and layers permutations
+    models = []
 
-node_options = list(range(min_num_nodes, max_num_nodes+1))
+    #forloops that combine layers and nodes to create the various model architectures. This is the model level where we setup the model and the input layer
+    for permutation in layer_node_permutations:
+    
+        model_name = ''
+        model = tf.keras.Sequential()
+        model.add(tf.keras.layers.InputLayer(input_shape=(8,)))
+    	
+    	#this is the layer level where for each layer we add the layers and the corresponding nodes
+        for nodes_in_layer in permutation:
+        
+            model.add(tf.keras.layers.Dense(nodes_in_layer, activation = 'relu'))
+            model_name += f'dense{nodes_in_layer}_'
+        
+        #finally we have the output layer and we append the model in the list
+        model.add(tf.keras.layers.Dense(2, activation = 'linear'))
+        model._name = model_name[:-1]
+    
+        models.append(model)
+    
+    return models
 
-layer_possibilities = num_layers*[node_options]
+models = get_models(num_layers = 2,
+                   min_num_nodes=3,
+                   max_num_nodes=7,
+                   node_step_size=1,
+                   hidden_layer_activation='relu',
+                   num_nodes_at_output=2,
+                   output_layer_activation='linear')
 
-layer_node_permutations = list(itertools.product(*layer_possibilities))
-print(layer_node_permutations)
-
-
-'''
-model = Sequential(
-    [
-        Dense(8, activation = 'relu', name = "L1"),
-        Dense(4, activation = 'relu', name = "L2"),
-        Dense(2, activation = 'linear', name = "L3")
-    ]
-)
-
-model.compile(
-    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-    optimizer=tf.keras.optimizers.Adam(0.01), metrics=['accuracy']
-)
-
-model.fit(
-    X_train,y_train,
-    epochs=10, validation_data=(X_test,y_test)
-)
-'''
+for model in models:
+	print(model.summary())
